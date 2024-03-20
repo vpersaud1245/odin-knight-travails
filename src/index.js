aa2ea4cc9ff2d8cb46f3db5a0ca2da44f447b05b
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-03-20 17:36:47 -0400

    Add getAllMoves() function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import "./style.css";
+
+function getAllMoves(coordinates) {
+    const moveArray = []
+    moveArray.push([coordinates[0]+ 2, coordinates[1] + 1])
+    moveArray.push([coordinates[0]+ 2, coordinates[1] - 1])
+    moveArray.push([coordinates[0]+ 1, coordinates[1] + 2])
+    moveArray.push([coordinates[0]+ 1, coordinates[1] - 2])
+    moveArray.push([coordinates[0] - 2, coordinates[1] + 1])
+    moveArray.push([coordinates[0] - 2, coordinates[1] - 1])
+    moveArray.push([coordinates[0] - 1, coordinates[1] + 2])
+    moveArray.push([coordinates[0] - 1, coordinates[1] - 2])
+    return moveArray.filter(coordinate => coordinate[0] <= 7 && coordinate[0] >= 0 && (coordinate[1] <= 7 && coordinate[1] >= 0));
+}
+
+console.log(getAllMoves([1,1]))
